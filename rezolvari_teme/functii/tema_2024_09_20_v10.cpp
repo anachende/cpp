fd387c8d1b27f252bfbd8fe6e46ff6019736c473
crchende <57460107+crchende@users.noreply.github.com>
2024-09-23 12:45:03 +0300

    Update tema_2024_09_20_v10.cpp

--- rezolvari_teme/functii/tema_2024_09_20_v10.cpp
+++ rezolvari_teme/functii/tema_2024_09_20_v10.cpp
@@ -1,49 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void divizori_primi(int nr) {
+void divizori_la_p_comumi(int nr1, int nr2) {
 	int i;
-	int p = 0;
-	int d = 2;
-	while(nr > 1) {
-		if(nr%d == 0) {
-			p += 1;
-			nr = nr/d;
-			//cout << "nr = " << nr << " d = " <<d<<endl;
-		} else {
-			//cout << "aici " << nr << " " <<d<< endl;
-			if(p != 0) {
-				cout << d << " la " << p << endl;
-			}
-			d++;
-			p = 0;
-		}
-	}
-	if(p != 0) {
-		cout << d << " la " << p << endl;
-	}
-}
-
-divizori_la_p_comumi(int nr1, int nr2) {
-	int i;
-	int p = 0;
+	int p1 = 0;
+	int p2 = 0;
 	int d = 2;
 	while(nr1 > 1 && nr2 > 1) {
-		if(nr1%d == 0) {
-			p += 1;
-			nr = nr/d;
-			//cout << "nr = " << nr << " d = " <<d<<endl;
-		} else {
-			//cout << "aici " << nr << " " <<d<< endl;
-			if(p != 0) {
-				cout << d << " la " << p << endl;
-			}
-			d++;
-			p = 0;
+		while(nr1%d == 0) {
+			p1 += 1;
+			nr1 = nr1/d;
+			//cout << "nr1 = " << nr1 << " d = " <<d<<endl;
 		}
+		while(nr2%d == 0) {
+			p2 += 1;
+			nr2 = nr2/d;
+			//cout << "nr2 = " << nr2 << " d = " <<d<<endl;
+		}
+		
+		if(p1 == p2 & p1 != 0) {
+			cout << "ok: " << d << "^" << p1 << endl;
+		} else if(p1 != p2) {
+			cout << "  divizori la puteri diferite:" 	<< endl;
+			cout << "  - nr1: " << d << "^" << p1 << endl;
+			cout << "  - nr2: " << d << "^" << p2 << endl;
+		}
+		d++;
+		p1 = 0;
+		p2 = 0;
 	}
-	if(p != 0) {
-		cout << d << " la " << p << endl;
+	/*
+	cout << "Afisare pentru verificare la iesirea din while:";
+	if(nr1 == 1 || nr2 == 1) {
+		cout << ":" 	<< endl;
+		cout << "- nr1: " << nr1 << " d = " << d << endl;
+		cout << "- nr2: " << nr2 << " d = " << d << endl;
+	}
+	*/
+	
+	//dubla verificare
+	if(p1 == p2 & p1 != 0) {
+		cout << d << " la " << p1 << endl;
+	} else if(p1 != p2) {
+			cout << "  divizori la puteri diferite:" 	<< endl;
+			cout << "  - nr1: " << d << " la " << p1 << endl;
+			cout << "  - nr2: " << d << " la " << p2 << endl;
 	}
 }
 
@@ -56,9 +57,10 @@ int main() {
 	
 	n = 16500;
 	m = 10780;
+	//n = 24; 	//2^3 * 3
+	//m = 120;	//2^3 * 3 * 5
 	
-	divizori_primi(n);
-	divizori_primi(m);
+	divizori_la_p_comumi(n, m);
 	
 	return 0;
 }
