e22c0d75d5c04150993eb137719cabef8f79a13f
anachende <ana22.chende@gmail.com>
2024-12-09 23:26:52 +0200

    Update felicitare.cpp

--- studiu/felicitare.cpp
+++ studiu/felicitare.cpp
@@ -2,12 +2,16 @@
 #include <string.h>
 using namespace std;
 int main() {
-    int coduri[11] = {86, 97, 32, 105, 117, 98, 105, 109, 33, 33, 33};
+    char coduri[12] = {86, 97, 32, 105, 117, 98, 105, 109, 33, 33, 33};
     string mesaj;
+    //char mesaj[20];
     for (int i = 0; i < 11; i++) {
         mesaj += static_cast<char>(coduri[i]);
+        //mesaj[i] = co
     }
     cout << mesaj << endl;
+    coduri(12) = '\0';
+    cout << coduri << endl;
 
     return 0;
 }
