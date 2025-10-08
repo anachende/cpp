62dc0cce50c3c1cae4a4e6e1e3eea54f85a3dd15
cradina cradina <ana22.chende@gmail.com>
2025-10-08 21:41:38 +0300

    bac1_bac2_txt

--- cl_11/pt_test1/bac1_bac2_txt.cpp
+++ cl_11/pt_test1/bac1_bac2_txt.cpp
@@ -2,44 +2,47 @@
 #include <fstream>
 
 //https://brainly.ro/tema/8388670
-//de validat, copiata de mai sus
+//de validat, copi1ta de mai sus
 
 using namespace std;
 int main(){
-   ifstream a("bac1.txt");
-   ifstream b("bac2.txt");
+   ifstream f1("bac1.txt");
+   ifstream f2("bac2.txt");
 
-   int xa,xb,ia=1,ib=1,na,nb;
-   a >> na >> xa;
-   b >> nb >> xb;
-   while(ia <= na && ib <= nb){
-       if(xa==xb) {
-           a >> xa;
-           b >> xb;
-           ia++;
-           ib++;
+   int x1,x2,i1=1,i2=1,n1,n2;
+   f1 >> n1 >> x1;
+   f2 >> n2 >> x2;
+   while(i1 <= n1 && i2 <= n2){
+       if(x1==x2) {
+           f1 >> x1;
+           f2 >> x2;
+           i1++;
+           i2++;
        }
-       if(xa<xb){
-           if(xa%5==0) cout << xa << " ";
-           a >> xa;
-           ia++;
+       if(x1<x2){
+           if(x1%5==0) cout << x1 << " ";
+           f1 >> x1;
+           i1++;
        }
-       if(xb<xa){
-           if(xb%5==0) cout << xb << " ";
-           b >> xb;
-           ib++;
+       if(x2<x1){
+           if(x2%5==0) cout << x2 << " ";
+           f2 >> x2;
+           i2++;
        }
    }
 
-   while(ia <= na){
-       a >> xa;
-       ia++;
-       if(xa%5==0) cout << xa << " ";
+   while(i1 <= n1){
+       if(x1%5==0) cout << x1 << " ";
+       //cout << "i1 =" << i1 << endl;
+       f1 >> x1;
+       //cout << "x1 =" << x1 << endl;
+       i1++;
    }
 
-   while(ib <= nb){
-       b >> xb;
-       ib++;
-       if(xb%5==0) cout << xb << " ";
+   while(i2 <= n2){
+       f2 >> x2;
+       i2++;
+       if(x2%5==0) cout << x2 << " ";
    }
+   return 0;
 }
